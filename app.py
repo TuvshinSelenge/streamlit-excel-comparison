5b4e65abe4733dc1bf447ee4426e10ec94d1cd6d
Tuvshin Selenge <132197695+TuvshinSelenge@users.noreply.github.com>
2024-07-25 10:31:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ import io
 import streamlit as st
 import logging
 
+st.set_page_config(
+    page_title="Data Comparison Tool",
+    layout="centered",
+    page_icon="https://upload.wikimedia.org/wikipedia/de/e/eb/Raiffeisen_%C3%96sterreich_logo.svg",
+)
+
+
 # Set up logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
@@ -212,11 +219,6 @@ def apply_conditional_formatting(output, sheet_name='Sheet2', column='Difference
 
 # Streamlit UI
 
-st.set_page_config(
-    page_title="Data Comparison Tool",
-    layout="centered",
-    page_icon="https://upload.wikimedia.org/wikipedia/de/e/eb/Raiffeisen_%C3%96sterreich_logo.svg",
-)
 
 def set_page_title(title):
     st.sidebar.markdown(
